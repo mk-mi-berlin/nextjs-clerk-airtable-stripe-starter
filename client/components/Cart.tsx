bf30572487dcaedfc8ff40738b618e34513c8d0f
matkl1 <mk@medieninformatik.berlin>
2023-05-07 13:49:47 +0200

    shoudlPersitst2

--- client/components/Cart.tsx
+++ client/components/Cart.tsx
@@ -3,10 +3,11 @@ import { CartProvider } from 'use-shopping-cart';
 
 const Cart = ({ children }: { children: ReactNode }) => (
     <CartProvider
+        shouldPersist={true}
         cartMode="checkout-session"
         stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!} // this isn't actually being used for Stripe Checkout
         currency={process.env.NEXT_PUBLIC_CURRENCY!}
-        shouldPersist={true}
+        
     >
         <>{children}</>
     </CartProvider>
